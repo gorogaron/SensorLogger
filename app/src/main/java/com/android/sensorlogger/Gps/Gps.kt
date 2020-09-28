978c52d09e420b34b18ac749c22ec12e92f8a858
Görög Áron <gorogaron95@gmail.com>
2020-09-28 22:05:50 +0200

    Switched to fused GPS provider

--- app/src/main/java/com/android/sensorlogger/Gps/Gps.kt
+++ app/src/main/java/com/android/sensorlogger/Gps/Gps.kt
@@ -1,30 +1,24 @@
 package com.agrolytics.agrolytics_android.utils
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Context.LOCATION_SERVICE
-import android.content.pm.PackageManager
+import android.location.Criteria
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat.getSystemService
+import android.widget.Toast
 import com.android.sensorlogger.App
 import com.android.sensorlogger.Utils.Logger
 import com.android.sensorlogger.Utils.PermissionHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import okhttp3.MediaType
-import okhttp3.RequestBody
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStreamWriter
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class Gps(context: Context) : LocationListener, Logger(context, "GPS")
 {
@@ -33,7 +27,7 @@ class Gps(context: Context) : LocationListener, Logger(context, "GPS")
     @SuppressLint("MissingPermission")
     fun run(){
         if (PermissionHelper.hasGpsPermission(context)) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0f, this)
+            locationManager.requestLocationUpdates("fused", 5000, 0f, this)
             initLogFile()
             startPeriodicUpload()
         }
@@ -45,14 +39,14 @@ class Gps(context: Context) : LocationListener, Logger(context, "GPS")
     }
 
     override fun onLocationChanged(loc: Location) {
-        Log.d("GPS", "${loc.latitude} ${loc.longitude}")
-
         //Low rate, so can be done in every iteration
         if (App.inMovement){
+            Toast.makeText(context, "GPS FOUND", Toast.LENGTH_LONG).show()
             GlobalScope.launch(Dispatchers.IO) {
                 val line = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss_SSS", Locale.US).format(Date()) + ":" +
                         loc.latitude.toString() + ";"
                         loc.longitude.toString() + "\n"
+                Log.d("GPS", "Logging coordinates: ${loc.latitude} ${loc.longitude}")
                 writeToFile(line)
                 closeFile()
             }
