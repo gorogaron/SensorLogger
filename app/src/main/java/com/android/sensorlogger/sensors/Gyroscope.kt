380dc7afbe2f1e3c0a5b935202463ba95cc235c3
Julian <dreerr@gmail.com>
2020-10-05 14:13:20 +0200

    Remove Underscores

--- app/src/main/java/com/android/sensorlogger/sensors/Gyroscope.kt
+++ app/src/main/java/com/android/sensorlogger/sensors/Gyroscope.kt
@@ -2,10 +2,7 @@ package com.android.sensorlogger.sensors
 
 import android.content.Context
 import android.hardware.Sensor
-import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
-import android.hardware.SensorManager
-import android.util.Log
 import com.android.sensorlogger.Utils.Config
 
 class Gyroscope(context: Context, fileName: String) : SensorEventListener, SensorBase(context, fileName) {
@@ -14,8 +11,8 @@ class Gyroscope(context: Context, fileName: String) : SensorEventListener, Senso
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
         sampleRateMillis = 500
 
-        x_threshold = Config.Sensor.GYRO_X_THRESHOLD
-        y_threshold = Config.Sensor.GYRO_Y_THRESHOLD
-        z_threshold = Config.Sensor.GYRO_Z_THRESHOLD
+        thresholdX = Config.Sensor.GYRO_X_THRESHOLD
+        thresholdY = Config.Sensor.GYRO_Y_THRESHOLD
+        thresholdZ = Config.Sensor.GYRO_Z_THRESHOLD
     }
 }
--- app/src/main/java/com/android/sensorlogger/sensors/Magnetometer.kt
+++ app/src/main/java/com/android/sensorlogger/sensors/Magnetometer.kt
@@ -2,10 +2,7 @@ package com.android.sensorlogger.sensors
 
 import android.content.Context
 import android.hardware.Sensor
-import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
-import android.hardware.SensorManager
-import android.util.Log
 import com.android.sensorlogger.Utils.Config
 
 class Magnetometer(context: Context, fileName: String) : SensorEventListener, SensorBase(context, fileName) {
@@ -14,8 +11,8 @@ class Magnetometer(context: Context, fileName: String) : SensorEventListener, Se
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
         sampleRateMillis = 500
 
-        x_threshold = Config.Sensor.MAG_X_THRESHOLD
-        y_threshold = Config.Sensor.MAG_Y_THRESHOLD
-        z_threshold = Config.Sensor.MAG_Z_THRESHOLD
+        thresholdX = Config.Sensor.MAG_X_THRESHOLD
+        thresholdY = Config.Sensor.MAG_Y_THRESHOLD
+        thresholdZ = Config.Sensor.MAG_Z_THRESHOLD
     }
 }
