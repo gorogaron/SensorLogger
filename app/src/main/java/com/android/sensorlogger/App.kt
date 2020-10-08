b4321f2cc0cf1182203d5bef054a9416b71369f0
Julian <dreerr@gmail.com>
2020-10-08 18:45:33 +0200

    correct Utils

--- app/src/main/java/com/android/sensorlogger/App.kt
+++ app/src/main/java/com/android/sensorlogger/App.kt
@@ -1,7 +1,7 @@
 package com.android.sensorlogger
 
 import android.app.Application
-import com.android.sensorlogger.utils.SessionManager
+import com.android.sensorlogger.Utils.SessionManager
 import com.android.sensorlogger.networking.UploadManager
 
 class App : Application() {
--- app/src/main/java/com/android/sensorlogger/networking/UploadManager.kt
+++ app/src/main/java/com/android/sensorlogger/networking/UploadManager.kt
@@ -2,8 +2,8 @@ package com.android.sensorlogger.networking
 
 import android.content.Context
 import android.util.Log
-import com.android.sensorlogger.utils.TAG
-import com.android.sensorlogger.utils.Util
+import com.android.sensorlogger.Utils.TAG
+import com.android.sensorlogger.Utils.Util
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
